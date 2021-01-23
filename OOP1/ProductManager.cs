e9f8aeb451971535780b4675df3525108b9dc5de
gokcen keskin <keskin.gokcen@gmail.com>
2021-01-23 08:57:26 +0000

    20.01.2021

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+     //encapsule
+        public void Add(Product product)//101
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+       
+
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "guncellendi.");
+        }
+        public int Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public int Topla2(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+    }
+}
